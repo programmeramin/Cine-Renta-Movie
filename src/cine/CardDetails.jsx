62e822f0fedfd1daccd0bb57f75056cd44be77a6
Programmeramin <developeramin2615@gmail.com>
2025-05-29 14:38:54 +0600

    movie cart add & cart to remove ui functionality done

--- src/cine/CardDetails.jsx
+++ src/cine/CardDetails.jsx
@@ -1,60 +1,82 @@
-import React from "react";
+import React from "react"
 import { useContext } from "react";
+import Delete from "../assets/delete.svg";
+import Checkout from "../assets/icons/checkout.svg";
 import { MovieContext } from "../context";
+import { getImgUrl } from "../utils/cine-utility";
 
 const CardDetails = ({ onClose }) => {
   const { cartData, setCartData } = useContext(MovieContext);
-  console.log(cartData);
+
+  function handleDeleteCart(event, itemId) {
+    
+    event.preventDefault();
+    const filteredItem = cartData.filter((item) =>{
+        return item.id !== itemId
+    });
+
+    setCartData([...filteredItem]);
+
+  }
+
 
   return (
     <>
       <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[984px] p-4 max-h-[90vh] overflow-auto">
-        
-          {cartData.map(item => (
-              <div className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl sm:grid sm:grid-cols-[2fr_1fr] overflow-hidden">
-            <img
-              className="sm:order-2 w-full object-cover h-full max-sm:max-h-[300px]"
-              src="./assets/movie-1.png"
-              alt=""
-            />
-            <div className="p-5 lg:p-11">
-              <div className="">
-                <h2 className="text-3xl lg:text-[50px] mb-2 font-bold">
-                  {item.title}
-                </h2>
-                <span className="block text-base text-[#9fa0a4] dark:text-[#575A6E] my-3">
-                  Action/Adventure/Sci-fi
-                </span>
-                <div />
-              </div>
-              <p className="text-sm lg:text-base mb-8 lg:mb-16">
-                When Branch’s brother, Floyd, is kidnapped for his musical
-                talents by a pair of nefarious pop-star villains, Branch and
-                Poppy embark on a harrowing and emotional journey to reunite the
-                other brothers and rescue Floyd from a fate even worse than
-                pop-culture obscurity.
-              </p>
-              <div className="grid lg:grid-cols-2 gap-2">
-                <a
-                  className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                  href="#"
-                >
-                  <img src="./assets/tag.svg" alt="" />
-                  <span>$100 | Add to Cart</span>
-                </a>
-                <a
-                  className="border border-[#74766F] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#6F6F6F] dark:text-gray-200 font-semibold text-sm"
-                  href="#"
-                  onClick={onClose}
-                >
-                  Cancel
-                </a>
-              </div>
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto">
+          <div className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl overflow-hidden p-5 md:p-9">
+            <h2 className="text-2xl lg:text-[30px] mb-10 font-bold">
+              Your Carts
+            </h2>
+            <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
+              {cartData.map((item) => (
+                <div className="grid grid-cols-[1fr_auto] gap-4">
+                  <div className="flex items-center gap-4">
+                    <img
+                      className="rounded overflow-hidden"
+                      src={getImgUrl(item.cover)}
+                      alt={item.title}
+                      width={"50px"}
+                      height={"50px"}
+                    />
+                    <div>
+                      <h3 className="text-base md:text-xl font-bold">
+                        {item.title}
+                      </h3>
+                      <p className="max-md:text-xs text-[#575A6E]">
+                        {item.genre}
+                      </p>
+                      <span className="max-md:text-xs">$1{item.price}</span>
+                    </div>
+                  </div>
+                  <div className="flex justify-between gap-4 items-center">
+                    <button className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white"
+                     onClick={() => handleDeleteCart(event, item.id)}
+                    >
+                      <img className="w-5 h-5" src={Delete} alt="" />
+                      <span className="max-md:hidden">Remove</span>
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="flex items-center justify-end gap-2">
+              <a
+                className="rounded-md p-2 md:px-4 inline-flex items-center space-x-2 bg-primary text-[#171923] text-sm"
+                href="#"
+              >
+                <img src={Checkout} width={24} height={24} alt="chckout" />
+                <span>Checkout</span>
+              </a>
+              <a
+                className="border border-[#74766F] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#6F6F6F] dark:text-gray-200 font-semibold text-sm"
+                href="#"
+                onClick={onClose}
+              >
+                Cancel
+              </a>
             </div>
           </div>
-          ))}
-
         </div>
       </div>
     </>
